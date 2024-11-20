6f9b5330012611428aca05dbad53786f0847acae
cartergi <cartergi@OA-S141-19>
2024-11-20 12:26:56 -0500

    Week 11 Class, Fixed Movement, Added, Jump (added cinemachine for fun)

--- Carter_Platformer/Assets/Scripts/PlayerController.cs
+++ Carter_Platformer/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float speed = 2;
 
     public enum FacingDirection
     {
         left, right
     }
 
+    public Rigidbody2D rb;
+    private float acceleration;
+
+    public float maxSpeed;
+    public float timeToReachMaxSpeed;
+
+    private float gravity;
+    private float initialJumpVel;
+
+    public float apexHeight;
+    public float apexTime;
+
+    public float terminalFallSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        acceleration = maxSpeed / timeToReachMaxSpeed;
     }
 
     // Update is called once per frame
@@ -26,22 +40,32 @@ public class PlayerController : MonoBehaviour
         
         Vector2 playerInput = new Vector2();
         MovementUpdate(playerInput);
+
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        Vector2 currentVelocity = rb.velocity;
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            playerInput = new Vector2(-1, 0) * speed;
-            rb.AddForce(playerInput);
+            currentVelocity += acceleration * Time.deltaTime * Vector2.left;
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            playerInput = new Vector2(1, 0) * speed;
-            rb.AddForce(playerInput);
+            currentVelocity += acceleration * Time.deltaTime * Vector2.right;
         }
+
+        if (Input.GetKey(KeyCode.Space) && IsGrounded() == true)
+        {
+            gravity = -2 * apexHeight / (Mathf.Pow(apexTime, 2));
+
+            initialJumpVel = 2 * apexHeight / apexTime;
+            currentVelocity += new Vector2(0, initialJumpVel);
+        }
+
+        rb.velocity = currentVelocity;
     }
 
     public bool IsWalking()
